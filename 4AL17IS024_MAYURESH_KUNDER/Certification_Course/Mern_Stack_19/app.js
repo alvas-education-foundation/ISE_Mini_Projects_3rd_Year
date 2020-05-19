56e878d36737120a117ffa276d52fe942f8beff2
mayur249 <kundermayuresh249@gmail.com>
2020-05-19 18:21:51 +0530

    Day 2

---
+++ 4AL17IS024_MAYURESH_KUNDER/Certification_Course/Mern_Stack_19/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.js");
+
+//DB connections
+mongoose
+.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => {
+    console.log("DB CONNECTED");
+});
+
+///middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+
+//Port
+ const port = process.env.PORT || 8000;
+
+ //Starting a server
+ app.listen(port, () => {
+     console.log(`app is running at ${port}`);
+ });
