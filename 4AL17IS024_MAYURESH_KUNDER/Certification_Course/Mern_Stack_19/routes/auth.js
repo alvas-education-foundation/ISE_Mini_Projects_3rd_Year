36f980acbc05e7c31b191cccb9b762d6b9df327a
mayur249 <kundermayuresh249@gmail.com>
2020-05-20 18:16:46 +0530

    Mini_Projects

--- 4AL17IS024_MAYURESH_KUNDER/Certification_Course/Mern_Stack_19/routes/auth.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const {signout, signup} = require("../controllers/auth");
-
-router.post("/signup", signup)
-router.get('/signout', signout);
-
-module.exports = router;
